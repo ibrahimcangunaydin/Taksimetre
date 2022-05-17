0d517944d3e3b3372e45712258e33c3be5418d3d
Ibrahim Can Gunaydin <ibrahimcangunaydin@gmail.com>
2022-05-17 16:44:40 +0300

    Taksimetre added

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int km;
+        double perKm=2.20, total, startPrice=10;
+
+        Scanner girdi = new Scanner(System.in);
+        System.out.print("Gidilen kilometreyi giriniz: ");
+        km = girdi.nextInt();
+
+        total =(km*perKm);
+        total +=startPrice;
+
+        total = (total<20) ? 20:total;
+
+        System.out.print("Odenecek Tutar: "+total);
+
+    }
+}
